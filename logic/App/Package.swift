20af6223a9894e26851f7220139c43940368422a
Denis Morozov <denis.moroz.morozov@gmail.com>
2020-08-16 21:31:43 +0300

    Init template

---
+++ logic/App/Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "App",
+    dependencies: [
+        .package(path: "../__BOTNAME__"),
+        .package(url: "https://github.com/dmoroz0v/TgBotSDK.git", from: "__TAG__"),
+    ],
+    targets: [
+        .target(
+            name: "App",
+            dependencies: [
+                "__BOTNAME__",
+                "TgBotSDK",
+            ]
+        ),
+    ]
+)
---
+++ logic/App/Sources/App/main.swift
@@ -0,0 +1,46 @@
+import Foundation
+import __BOTNAME__
+import ChatBotSDK
+import TgBotSDK
+import SQLite
+
+func shell(_ args: [String]) -> [String] {
+    var result : [String] = []
+    let process = Process()
+    process.launchPath = "/usr/bin/env"
+    process.arguments = args
+    let standardOutput = Pipe()
+    process.standardOutput = standardOutput
+    process.launch()
+    let resultdata = standardOutput.fileHandleForReading.readDataToEndOfFile()
+    if var stringValue = String(data: resultdata, encoding: .utf8) {
+        stringValue = stringValue.trimmingCharacters(in: .newlines)
+        result = stringValue.components(separatedBy: "\n")
+    }
+    process.waitUntilExit()
+    return result.map { $0.trimmingCharacters(in: .whitespacesAndNewlines) }
+
+}
+
+struct Config: Decodable {
+	let telegram_bot_token: String
+}
+
+let basePath = URL(fileURLWithPath: shell(["pwd"]).first!)
+let configPath = basePath.appendingPathComponent("../config.json")
+
+let config = try! JSONDecoder().decode(Config.self, from: Data(contentsOf: configPath))
+
+let b = TgBotSDK.Bot(
+    flowStorage: try! FlowStorageImpl(),
+    botAssembly: BotAssemblyImpl(),
+    token: config.telegram_bot_token,
+    apiEndpoint: "https://api.telegram.org/bot")
+
+DispatchQueue.global().async {
+    while true {
+        b.handleUpdates()
+    }
+}
+
+dispatchMain()
